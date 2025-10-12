d088060f4b930a26302cba62b58c4dedc6c8770a
iamVinayak50 <vinayakshegar50@gmail.com>
2025-10-12 23:04:33 +0530

    initial commit

---
+++ dags/bq_to_bq_dag.py
@@ -0,0 +1,39 @@
+from airflow import DAG
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from datetime import datetime
+
+default_args = {
+    'start_date': datetime(2025, 10, 13),
+    'retries': 1,
+}
+
+with DAG(
+    dag_id='daily_bq_transfer_data_cross_project',
+    schedule_interval='@daily',
+    default_args=default_args,
+    catchup=False,
+    tags=['bigquery', 'cross_project'],
+) as dag:
+
+    transfer_raw_car_specs = BigQueryInsertJobOperator(
+        task_id='transfer_data_cross_project',
+        configuration={
+            "query": {
+                "query": """
+                    CREATE OR REPLACE TABLE `boxwood-axon-470816-b1.bronze_dataset.raw_car_specs`
+                    AS
+                    SELECT * FROM `helical-gist-445906-k3.bronze_dataset.raw_car_specs`
+                    
+                    CREATE OR REPLACE TABLE `boxwood-axon-470816-b1.bronze_dataset.raw_dealer_info`
+                    AS
+                    SELECT * FROM `helical-gist-445906-k3.bronze_dataset.raw_dealer_info`
+                    
+                    CREATE OR REPLACE TABLE `boxwood-axon-470816-b1.bronze_dataset.raw_sales_data`
+                    AS
+                    SELECT * FROM `helical-gist-445906-k3.bronze_dataset.raw_sales_data`
+                """,
+                "useLegacySql": False,
+            }
+        },
+        location='US',  # or your region
+    )
